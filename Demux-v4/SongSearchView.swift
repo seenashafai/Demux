8afc10af04682885876fcfd13028169d5410f092
Seena Shafai <seena@shafai.com>
2020-07-07 03:19:32 +0200

    Implemented list of results of song search query.

--- Demux-v4/SongSearchView.swift
+++ Demux-v4/SongSearchView.swift
@@ -12,44 +12,44 @@ import SwiftyJSON
 struct SongSearchView: View {
     
     @State var searchText: String
+    @State var songArray = [Song]()
+
     
     var body: some View {
         VStack{
-            var songArray = [Song]()
             Text("Search for a song...")
                 .font(.system(size: 30, weight: .black, design: .default))
                 .padding(10)
+            
             SearchBar(text: $searchText)
-                Spacer()
+            Spacer()
             
             Button(action: {
+                //Init query and token for track search
                 let query = searchText
                 let authToken = Session.globalSession.authToken
-                print(query, authToken)
-                var songArray = trackSearch(query: query, authToken: authToken!)
-                print(songArray, "returned")
+                //API request for song results
+                trackSearch(query: query, authToken: authToken!) { songs in
+                    songArray = songs
+                }
                 
                 
             }) {
                 Text("Search")
             }
-            List{
-//                SongRow(song: songArray[0])
-//                SongRow(song: songArray[1])
-//                SongRow(song: songArray[2])
-//                SongRow(song: songArray[3])
-//                SongRow(song: songArray[4])
+            List(songArray) { song in
+                SongRow(song: song)
             }
             
             
-        }
     }
     // Dismiss the keyboard
 
        
 }
+}
 
-func trackSearch(query: String, authToken: String) -> [Song] {
+func trackSearch(query: String, authToken: String, completion: @escaping ([Song]) -> Void) {
     var urlParameters = URLComponents(string: "https://api.spotify.com/v1/search/")!
     var resultsArray = [Song]()
 
@@ -59,18 +59,17 @@ func trackSearch(query: String, authToken: String) -> [Song] {
         URLQueryItem(name: "market", value: "GB"),
         URLQueryItem(name: "limit", value: "5")
     ]
-
     AF.request(urlParameters.url!, method: .get, encoding: JSONEncoding.default, headers: ["Authorization": "Bearer "+authToken]).responseJSON { response in
         switch response.result {
             case .success(let value):
                 let json = JSON(value)
                 resultsArray = arrangeSearchResults(json: json)
                 print(resultsArray)
+                completion(resultsArray)
             case .failure(let error):
                 print(error)
             }
     }
-    return resultsArray
 }
 
 func arrangeSearchResults(json: JSON) -> [Song] {
@@ -85,6 +84,7 @@ func arrangeSearchResults(json: JSON) -> [Song] {
         song.id = items[i]["uri"].string!
         resultsArray.append(song)
     }
+    print(resultsArray)
     return resultsArray
 }
 
